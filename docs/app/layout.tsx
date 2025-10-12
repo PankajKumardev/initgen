ce4109e8239b1decf2f3c70c2674aa790b7025aa
pankaj kumar <pankajamsali@gmail.com>
2025-10-13 01:38:31 +0530

    feat: add Vercel Analytics integration to layout and update dependencies

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'InitGen Documentation',
@@ -25,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <Analytics />
       <body>{children}</body>
     </html>
   );
